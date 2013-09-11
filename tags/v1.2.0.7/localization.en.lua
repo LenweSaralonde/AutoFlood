ebb6134f3df72cc50153e8e549a09b2dddd441e8
lenwe-saralonde <lenwe-saralonde@b495986f-70d7-4978-85fa-cba8e50e1623>
2013-09-11 18:28:12 +0000

    Patch 5.4 tag

---
+++ tags/v1.2.0.7/localization.en.lua
@@ -0,0 +1,26 @@
+-- Version : English (default) ( by @project-author@ )
+-- Last Update : 22/05/2006
+
+
+AUTOFLOOD_LOAD		= "AutoFlood VERSION loaded. Type /floodhelp for help."
+
+AUTOFLOOD_STATS 	= "\"MESSAGE\" is sent every RATE seconds in channel /CHANNEL."
+
+AUTOFLOOD_MESSAGE	= "The message is now \"MESSAGE\"."
+AUTOFLOOD_RATE		= "The message is now sent every RATE seconds."
+AUTOFLOOD_CHANNEL	= "The message is now sent in channel /CHANNEL."
+
+AUTOFLOOD_ACTIVE     	= "AutoFlood is enabled."
+AUTOFLOOD_INACTIVE   	= "AutoFlood is disabled."
+
+AUTOFLOOD_ERR_CHAN   	= "The channel /CHANNEL doesn't exist."
+AUTOFLOOD_ERR_RATE  	= "You can't send messages less than every RATE seconds."
+ 
+AUTOFLOOD_HELP		= {	"===================== Auto Flood =====================",
+				"/flood [on|off] : Start / stops sending the message.",
+				"/floodmsg <message> : Sets the message.",
+				"/floodchan <channel> : Sets the channel.",
+				"/floodrate <duration> : Sets the period (in seconds).",
+				"/floodinfo : Displays parameters.",
+				"/floodhelp : Displays this help message.",
+			  }
